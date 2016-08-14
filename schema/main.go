f71475e65fbd9069f411cf5772d72cd6ddb5fb56
tonykung <tonykung06@gmail.com>
2016-08-14 20:26:52 +0800

    custom model binding function for time.Time field

--- schema/main.go
+++ schema/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"time"
 
 	"github.com/gorilla/schema"
 )
@@ -10,12 +12,14 @@ func main() {
 	basicModelBinding()
 }
 
+//the decoder will cache schema info of the struct
 var decoder *schema.Decoder
 
 func basicModelBinding() {
 	form := map[string][]string{
 		"firstName":         []string{"Tony"},
 		"lastName":          []string{"Kung"},
+		"updatedAt":         []string{"2016-08-11"},
 		"addresses.0.city":  []string{"Hong Kong"},
 		"addresses.0.state": []string{"N/A"},
 		"addresses.1.city":  []string{"Hong Kong 2"},
@@ -26,6 +30,14 @@ func basicModelBinding() {
 
 	if decoder == nil {
 		decoder = schema.NewDecoder()
+		decoder.RegisterConverter(time.Now(), func(val string) reflect.Value {
+			result := reflect.Value{}
+			if t, err := time.Parse("2006-01-02", val); err == nil {
+				result = reflect.ValueOf(t)
+			}
+
+			return result
+		})
 	}
 
 	decoder.Decode(&p, form)
@@ -36,6 +48,7 @@ type Person struct {
 	FirstName string    `schema:"firstName"`
 	LastName  string    `schema:"-"` //skip this field
 	Addresses []Address `schema:"addresses"`
+	UpdatedAt time.Time `schema:"updatedAt"`
 }
 
 type Address struct {
