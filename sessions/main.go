b6c82ce2c11228e283cc735f0d3463f23bab9f72
tonykung <tonykung06@gmail.com>
2016-08-16 00:44:43 +0800

    storing flash msg in sessions

--- sessions/main.go
+++ sessions/main.go
@@ -9,9 +9,31 @@ import (
 )
 
 var store = sessions.NewCookieStore(securecookie.GenerateRandomKey(64))
+var fileSystemStore = sessions.NewFilesystemStore("/test_session", securecookie.GenerateRandomKey(64))
 
 func main() {
-	sessionDataStoredInCookie()
+	// sessionDataStoredInCookie()
+	// sessionDataStoredInFileSystem()
+	flashMessages()
+}
+
+func sessionDataStoredInFileSystem() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		session, _ := fileSystemStore.Get(r, "session-name")
+		session.Values["favorite-song"] = []string{"My Heart will go on", "Love"}
+		session.Save(r, w)
+		w.Write([]byte("<h1>Hello Sessions!</h1>"))
+	})
+	http.HandleFunc("/sessioncontents", func(w http.ResponseWriter, r *http.Request) {
+		session, _ := fileSystemStore.Get(r, "session-name")
+		songs := session.Values["favorite-song"].([]string)
+		w.Header().Add("Content-Type", "text/html")
+		for _, song := range songs {
+			w.Write([]byte(song + "<br />"))
+		}
+	})
+	//seems there are also some other places leaking memory
+	http.ListenAndServe(":3000", context.ClearHandler(http.DefaultServeMux))
 }
 
 func sessionDataStoredInCookie() {
@@ -25,6 +47,33 @@ func sessionDataStoredInCookie() {
 		session, _ := store.Get(r, "session-name")
 		songs := session.Values["favorite-song"].([]string)
 		w.Header().Add("Content-Type", "text/html")
+		for _, song := range songs {
+			w.Write([]byte(song + "<br />"))
+		}
+	})
+	//seems there are also some other places leaking memory
+	http.ListenAndServe(":3000", context.ClearHandler(http.DefaultServeMux))
+}
+
+func flashMessages() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "session-name")
+		session.Values["favorite-song"] = []string{"My Heart will go on", "Love"}
+		session.AddFlash("Flash messages only work once!")
+		session.Save(r, w)
+		w.Write([]byte("<h1>Hello Sessions!</h1>"))
+	})
+	http.HandleFunc("/sessioncontents", func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "session-name")
+		songs := session.Values["favorite-song"].([]string)
+		w.Header().Add("Content-Type", "text/html")
+		if flashes := session.Flashes(); len(flashes) > 0 {
+			session.Save(r, w)
+			for _, msg := range flashes {
+				m := msg.(string)
+				w.Write([]byte(m + "<br />"))
+			}
+		}
 		for _, song := range songs {
 			w.Write([]byte(song + "<br />"))
 		}
