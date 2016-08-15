abbd6a00d115b4fe470de982bb1713cf6ed7fb4d
tonykung <tonykung06@gmail.com>
2016-08-15 22:37:25 +0800

    JSON-RPC

---
+++ rpc/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
+)
+
+func main() {
+	go startServer()
+	go startClient()
+	fmt.Scanln()
+}
+
+func startClient() {
+	args := ClientArgs{4, 5}
+	msg, _ := json.EncodeClientRequest("ArithService.Add", args)
+	resp, _ := http.Post("http://localhost:1234/rpc", "application/json", bytes.NewReader(msg))
+	var result int
+	json.DecodeClientResponse(resp.Body, &result)
+	fmt.Println(result)
+}
+
+type ClientArgs struct {
+	A, B int
+}
+
+func startServer() {
+	server := rpc.NewServer()
+	server.RegisterCodec(json.NewCodec(), "application/json")
+	server.RegisterService(new(ArithService), "")
+	http.Handle("/rpc", server)
+	httpServer := &http.Server{
+		Addr:    ":1234",
+		Handler: nil,
+		ConnState: func(conn net.Conn, state http.ConnState) {
+			// fmt.Println(state)
+		},
+	}
+	httpServer.ListenAndServe()
+}
+
+type Args struct {
+	A, B int
+}
+
+type ArithService struct{}
+
+func (a *ArithService) Add(req *http.Request, args *Args, reply *int) error {
+	*reply = args.A + args.B
+	return nil
+}
--- securecookie/main.go
+++ securecookie/main.go
@@ -15,14 +15,14 @@ func main() {
 func authenticatedCookie() {
 	//in production, these keys should generate once and kept secret
 	privateHashKey := securecookie.GenerateRandomKey(64) //32 or 64
-	s := securecookie.New(privateHashKey, nil)
+	cookieCodec := securecookie.New(privateHashKey, nil)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		//to base64 decode the signed base64 encoded cookie, on browser, do atob("MTQ3MTE4NTg4OHxDZ3dBQjIxNWRtRnNkV1U9fJpSombuiu7w0MjZ1UOJOj9qqABVsScMkvW4ujoD2o8b".replace("-", "+").replace("_", "/"))
 		//we have two base64 encoding schema, the traditional one maps binary data to 64 ASCII characters that are safe to transmit without data loss --> a-z A-Z 0-9 + /
 		//but since + and / have special meanings to browser, so they are replaced with - and _ respectively to give a url friendly schema
 		//but browsers atob only understands the traditional one and golang is using the url friendly one, we need to do string replace
-		if signedEncodedCookie, err := s.Encode("mycookie", "myvalue"); err == nil {
+		if signedEncodedCookie, err := cookieCodec.Encode("mycookie", "myvalue"); err == nil {
 			cookie := http.Cookie{
 				Name:  "mycookie",
 				Value: signedEncodedCookie, //signed and base64 encoded, which is comprised of three parts: a timestamp(for expiry checking) | cookie value | hash signature = f(name, value, private key, timestamp)
